a6cb8b086c89e6d2814923f4e25c39d1df8fd08b
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-11-23 16:25:33 +0000

    Main app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+import React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/AuthenticationScreens/LoginScreen';
+import SignUpScreen from './screens/AuthenticationScreens/SignUpScreen';
+import Splash from './screens//HomeScreens/splashHome';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, Poppins_800ExtraBold} from '@expo-google-fonts/poppins';
+import * as Font from 'expo-font'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+let customFonts = {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+
+  state ={
+    fontsLoaded : false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({fontsLoaded: true});
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return null;
+    }
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
+          <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
+          <Stack.Screen name="SignUp" component={SignUpScreen} options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
