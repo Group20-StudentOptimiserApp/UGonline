7e8a23cbabed2e7a8b6f74b380fa7aef956610e9
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-12-18 10:55:37 +0000

    Your Profile Screen

---
+++ screens/MainAppScreens/Profile.js
@@ -0,0 +1,98 @@
+import { View, Text,StyleSheet, Pressable, ScrollView } from 'react-native'
+import { color, font } from '../../global/styles'
+import { Avatar } from '@rneui/themed'
+import { AntDesign } from '@expo/vector-icons'
+import { auth } from '../../firebase'
+const Profile = ({currentUser}) => {
+
+  const signOutUser = () => {
+    auth.signOut();
+  }
+  const userSettings = () => {
+
+  }
+  const userPersonalInformation = () => {
+
+  }
+  const userAcademicInformation = () => {
+
+  }
+  const userHelp = () => {
+
+  }
+  const privacynSecurity = () => {
+
+  }
+  const data = [
+    {id: 1, text: 'Personal Information', icon: 'user', press: userPersonalInformation},
+    {id: 2, text: 'Academic Information', icon: 'book', press: userAcademicInformation},
+    {id: 3, text: 'Settings', icon: 'setting', press: userSettings},
+    {id: 4, text: 'Help', icon: 'questioncircleo', press: userHelp},
+    {id: 5, text: 'Privacy and Security', icon: 'Safety', press: privacynSecurity},
+    {id: 6, text: 'Logout', icon: 'logout', press: signOutUser},
+  ]
+  return (
+    <View style={styles.Container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <View style={styles.firstContainer}>
+            <Text style={{textAlign:'center', fontFamily: font.medium, fontSize: 24}}>Profile</Text>
+            <Avatar
+              rounded
+              title= {currentUser.name.charAt(0)}
+              titleStyle={{fontFamily: font.semiBold, fontSize: 60, color: color.primary}}
+              size={140}
+              // activeOpacity = {0.7}
+              overlayContainerStyle={{backgroundColor: '#D3F5E3',}}
+              containerStyle={{marginVertical: 8}}
+            />
+            <Text style={{textAlign:'center', fontFamily: font.semiBold, fontSize: 26, }}>{currentUser.name}</Text>
+            <Text style={{textAlign:'center', fontFamily: font.regular, fontSize: 16}}>{currentUser.Programme}</Text>
+          </View>
+          <View style={styles.secondContainer}>
+              {data.map((data)=>{
+                const {id, text, icon, press} = data
+                return(
+                  <Pressable onPress={press} key={id} style={{flexDirection: 'row', marginBottom: 24, alignItems:'center'}}>
+                      <AntDesign
+                        name= {icon}
+                        color= {color.primary}
+                        size={32}
+                        style={{paddingRight: 12}}
+                      />
+                      <Text style={{fontFamily: font.regular, fontSize: 24}}>{text}</Text>
+                  </Pressable>
+                )
+              })}
+          </View>
+        </View>
+        </ScrollView>
+    </View>
+  )
+}
+
+export default Profile
+
+
+const styles = StyleSheet.create({
+  Container:{
+        flex: 1,
+        backgroundColor: color.background,
+        
+    },
+    container:{
+        flex: 1,
+        padding: 10,
+        marginTop: 28,
+        marginBottom: 36,
+    },
+    firstContainer:{
+        marginBottom: 16,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    secondContainer: {
+      width: "100%",
+      padding: 16,
+    }
+})
