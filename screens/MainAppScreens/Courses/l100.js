5c6310bc52f8c39a9e4a350302c50d2e023f0cce
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2024-01-15 14:58:11 +0000

    default courses for level 100 and 200

--- screens/MainAppScreens/Courses/l100.js
+++ screens/MainAppScreens/Courses/l100.js
@@ -17,6 +17,8 @@ const L100 = ({currentUser}) => {
     const [courseName, setCourseName] = useState('')
 
     const [courseData, setCourseData] = useState([])
+    const [defaultCourseData, setDefaultCourseData] = useState([])
+
 
     const getCourse = async ()  =>{
         const getCourseData = [];
@@ -33,8 +35,24 @@ const L100 = ({currentUser}) => {
     setCourseData(getCourseData);
     }
 
+    const getDefaultCourse = async ()  =>{
+        const getDefaultCourseData = [];
+
+        const querySnapshot = await getDocs(collection(db, "DefaultCourses", 'ForAll', "L100"));
+        
+        querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        getDefaultCourseData.push({
+            ...doc.data(),
+            key: doc.id,
+        });
+    });
+    setDefaultCourseData(getDefaultCourseData);
+    }
+
     useEffect(()=>{
         getCourse();
+        getDefaultCourse();
     }, [])
 
   return (
@@ -43,6 +61,38 @@ const L100 = ({currentUser}) => {
         <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
             <View style={styles.primaryContainer}>
+                {defaultCourseData.map((data)=>{
+                    return(
+                        <TouchableOpacity activeOpacity={0.7} key={data.key} style={styles.secondaryContainer} onPress={()=> {navigation.navigate('CourseDetails', {
+                            coursesCode: data['Course Code'],
+                            coursesName: data['Course Name'],
+                        })}} >
+                            <View style={{width: '15%',}}>
+                                <View style={{backgroundColor: color.dark, borderRadius: 4, width: 1, height: 1, padding: 18, justifyContent: 'center', alignItems: 'center',width: 1, alignSelf:'flex-start', marginRight: 6}}>
+                                    <Image
+                                        source={require('../../../assets/forcourses.png')}
+                                        style={{position: 'absolute', height: 24, width: 28}}
+                                        
+                                    />
+                                </View>
+                            </View>
+                            <View style={{width: '65%', }}>
+                                <Text style={{ fontFamily: font.semiBold, fontSize: 20, color: color.dark, includeFontPadding:false, marginTop: -4}}>{data['Course Code']}</Text>
+                                <Text style={{ fontFamily: font.medium, fontSize: 16, color: color.dark, includeFontPadding:false, marginTop: -4}}>{data['Course Name']}</Text>
+                            </View>
+                            <View style={{width: '18%'}}>
+                            <Pressable style={{backgroundColor: color.light, borderRadius: 4, height: 1, padding:16, justifyContent: 'center', alignItems: 'center',width: 1, alignSelf:'flex-end' }}>
+                                <Feather
+                                    name='chevron-right'
+                                    size={24}
+                                    style={{ position: 'absolute',}}
+                                />
+                            </Pressable>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })}
+
                 {courseData.map((data)=>{
                     return(
                         <TouchableOpacity activeOpacity={0.7} key={data.key} style={styles.secondaryContainer} onPress={()=> {navigation.navigate('CourseDetails', {
--- screens/MainAppScreens/Courses/l200.js
+++ screens/MainAppScreens/Courses/l200.js
@@ -15,6 +15,7 @@ const L200 = ({currentUser}) => {
     const [courseName, setCourseName] = useState('')
 
     const [courseData, setCourseData] = useState([])
+    const [defaultCourseData, setDefaultCourseData] = useState([])
 
     const getCourse = async ()  =>{
         const getCourseData = [];
@@ -31,8 +32,24 @@ const L200 = ({currentUser}) => {
     setCourseData(getCourseData);
     }
 
+    const getDefaultCourse = async ()  =>{
+        const getDefaultCourseData = [];
+
+        const querySnapshot = await getDocs(collection(db, "DefaultCourses", 'ForAll', "L200"));
+        
+        querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        getDefaultCourseData.push({
+            ...doc.data(),
+            key: doc.id,
+        });
+    });
+    setDefaultCourseData(getDefaultCourseData);
+    }
+
     useEffect(()=>{
         getCourse();
+        getDefaultCourse();
     }, [])
 
   return (
@@ -40,6 +57,38 @@ const L200 = ({currentUser}) => {
         <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
             <View style={styles.primaryContainer}>
+            {defaultCourseData.map((data)=>{
+                    return(
+                        <TouchableOpacity activeOpacity={0.7} key={data.key} style={styles.secondaryContainer} onPress={()=> {navigation.navigate('CourseDetails', {
+                            coursesCode: data['Course Code'],
+                            coursesName: data['Course Name'],
+                        })}} >
+                            <View style={{width: '15%',}}>
+                                <View style={{backgroundColor: color.dark, borderRadius: 4, width: 1, height: 1, padding: 18, justifyContent: 'center', alignItems: 'center',width: 1, alignSelf:'flex-start', marginRight: 6}}>
+                                    <Image
+                                        source={require('../../../assets/forcourses.png')}
+                                        style={{position: 'absolute', height: 24, width: 28}}
+                                        
+                                    />
+                                </View>
+                            </View>
+                            <View style={{width: '65%', }}>
+                                <Text style={{ fontFamily: font.semiBold, fontSize: 20, color: color.dark, includeFontPadding:false, marginTop: -4}}>{data['Course Code']}</Text>
+                                <Text style={{ fontFamily: font.medium, fontSize: 16, color: color.dark, includeFontPadding:false, marginTop: -4}}>{data['Course Name']}</Text>
+                            </View>
+                            <View style={{width: '18%'}}>
+                            <Pressable style={{backgroundColor: color.light, borderRadius: 4, height: 1, padding:16, justifyContent: 'center', alignItems: 'center',width: 1, alignSelf:'flex-end' }}>
+                                <Feather
+                                    name='chevron-right'
+                                    size={24}
+                                    style={{ position: 'absolute',}}
+                                />
+                            </Pressable>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })}
+                
                 {courseData.map((data)=>{
                     return(
                         <TouchableOpacity activeOpacity={0.7} key={data.key} style={styles.secondaryContainer} onPress={()=> {navigation.navigate('CourseDetails', {
